0324ea5cdeac0b195618a46db6c42ec7af69beaf
Baldwin Ngo <baldwinngo0813@gmail.com>
2022-05-16 21:52:54 -0700

    finish assignment 2b

---
+++ part2/2b_phonebook/src/App.js
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+
+const Person = ({ persons }) => {
+  return(
+    <p>{ persons.name } { persons.phone }</p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '1234567890' }
+  ]) 
+  const [filterPersons, setfilterPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  const personsToShow = showAll ? persons : filterPersons
+
+  const addPerson = e => {
+    e.preventDefault()
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already in the book fam`)
+    } else {
+      const newPerson = {name: newName, phone: newPhone}
+      setPersons(persons.concat(newPerson))
+    }
+    
+    setNewName('')
+    setNewPhone('')
+  }
+  const handleNewPerson = e => {
+    setNewName(e.target.value)
+    
+  }
+  const handleNewPhone = e => {
+    setNewPhone(e.target.value)
+  }
+  const handleFilter = e => {
+    if (e.target.value) {
+      setShowAll(false)
+    } else {
+      setShowAll(true)
+    }
+    setfilterPersons(persons.filter(person => person.name.toLowerCase().includes(e.target.value)))
+  }
+  
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={ addPerson }>
+        <div>
+          name: <input value={ newName } onChange={ handleNewPerson }/>
+        </div>
+        <div>
+          number: <input value={ newPhone } onChange={ handleNewPhone }/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <form>
+        <div>
+          filter: <input onChange={ handleFilter }/>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {personsToShow.map(person =>
+          <Person key={ person.name } persons={ person }/>  
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
