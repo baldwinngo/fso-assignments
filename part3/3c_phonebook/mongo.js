86058a2524589d3f6a3a932632764bcdd12798a4
Baldwin Ngo <baldwinngo0813@gmail.com>
2022-06-21 22:22:16 -0700

    adding mongo exercise in phonebook directory
    need to npm install mongoose

---
+++ part3/3c_phonebook/mongo.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://baldwinngo:${password}@cluster0.r2izu.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+  date: Date
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length === 3) {
+  mongoose
+    .connect(url)
+    .then((result) => {
+      console.log(`phonebook:`)
+      Person.find({}).then(result => {
+        result.forEach(person => {
+          console.log(`${person.name} ${person.number}`)
+        })
+      })
+    })
+}
+
+if (process.argv.length === 5) {
+  mongoose
+    .connect(url)
+    .then((result) => {
+      console.log('connected')
+
+      const person = new Person({
+        name: name,
+        number: number,
+        date: new Date(),
+      })
+
+      return person.save()
+    })
+    .then(() => {
+      console.log(`added ${name} number ${number} to phonebook`)
+      return mongoose.connection.close()
+    })
+    .catch((err) => console.log(err))
+}
