1250d604cdeccc61377a0162b624cf2e412decd0
Baldwin Ngo <baldwinngo0813@gmail.com>
2022-09-14 22:19:01 -0700

    changing key to name instead of id works for some reason...

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -103,7 +103,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {personsToShow.map(person => <li key={ person.id }>{ person.name } - { person.number } <button onClick={() => handleDelete(person.id) }>delete</button></li>)}
+        {personsToShow.map(person => <li key={ person.name }>{ person.name } - { person.number } <button onClick={() => handleDelete(person.id) }>delete</button></li>)}
       </ul>
     </div>
   )
