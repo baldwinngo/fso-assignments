9de63c3dc9c088a0bfa548b8046e2b298e7eb563
Baldwin Ngo <baldwinngo0813@gmail.com>
2022-09-21 17:00:10 -0700

    deploying to internet

--- part3example/node/index.js
+++ part3example/node/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
+app.use(cors())
 
 let notes = [
   {
@@ -78,7 +81,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
