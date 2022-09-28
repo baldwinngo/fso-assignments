b2fed4e28083b65957c50028110911d8944baf60
Baldwin Ngo <baldwinngo0813@gmail.com>
2022-09-27 17:18:26 -0700

    lint

---
+++ part4/.eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'commonjs': true,
+        'es2021': true,
+        'node': true
+    },
+    'extends': 'airbnb-base',
+    'rules': {
+        'linebreak-style': [
+            'error',
+            'windows'
+        ]
+    }
+}
+
+// module.exports = {
+//     'env': {
+//         'browser': true,
+//         'commonjs': true,
+//         'es2021': true,
+//         'node': true
+//     },
+//     'extends': 'eslint:recommended',
+//     'overrides': [
+//     ],
+//     'parserOptions': {
+//         'ecmaVersion': 'latest'
+//     },
+//     'rules': {
+//         'eqeqeq': 'error',
+//         'indent': [
+//             'error',
+//             2
+//         ],
+//         'linebreak-style': [
+//             'error',
+//             'windows'
+//         ],
+//         'quotes': [
+//             'error',
+//             'single'
+//         ],
+//         'semi': [
+//             'error',
+//             'never'
+//         ],
+//         'no-trailing-spaces': 'error',
+//         'object-curly-spacing': [
+//             'error', 'always'
+//         ],
+//         'arrow-spacing': [
+//             'error', { 'before': true, 'after': true }
+//         ],
+//         'no-console': 0
+//     }
+// }
