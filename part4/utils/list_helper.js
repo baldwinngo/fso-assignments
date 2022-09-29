9992e29bcfe617a52f57b43d363b0b581752f459
Baldwin Ngo <baldwinngo0813@gmail.com>
2022-09-29 15:14:30 -0700

    finish tests

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -12,7 +12,62 @@ const totalLikes = (blogs) => {
   return total;
 };
 
+const favoriteBlog = (blogs) => {
+  const max = Math.max(...blogs.map((blog) => blog.likes));
+
+  return blogs.find((blog) => blog.likes === max);
+};
+
+const mostBlogs = (blogs) => {
+  const authorCount = {};
+  let compare = 0;
+  let mostAuthor = '';
+
+  blogs.forEach((blog) => {
+    if (!authorCount[blog.author]) {
+      authorCount[blog.author] = 1;
+    } else {
+      authorCount[blog.author] += 1;
+    }
+    if (authorCount[blog.author] > compare) {
+      compare = authorCount[blog.author];
+      mostAuthor = blog.author;
+    }
+  });
+
+  return {
+    author: mostAuthor,
+    blogs: compare,
+  };
+};
+
+const mostLikes = (blogs) => {
+  const likesCount = {};
+  let compare = 0;
+  let mostAuthor = '';
+
+  blogs.forEach((blog) => {
+    if (!likesCount[blog.author]) {
+      likesCount[blog.author] = blog.likes;
+    } else {
+      likesCount[blog.author] += blog.likes;
+    }
+    if (likesCount[blog.author] > compare) {
+      compare = likesCount[blog.author];
+      mostAuthor = blog.author;
+    }
+  });
+
+  return {
+    author: mostAuthor,
+    likes: compare,
+  };
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
